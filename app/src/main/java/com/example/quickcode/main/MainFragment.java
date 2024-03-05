f0059ef6fd66cfb08548b26a1601823933f0de5f
xFilipW <Filipo-filipo2003@wp.pl>
2024-03-05 21:54:26 +0100

    feat: create new bottom nav bar and general code refactor

--- app/src/main/java/com/example/quickcode/main/MainFragment.java
+++ app/src/main/java/com/example/quickcode/main/MainFragment.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.quickcode.R;
 import com.example.quickcode.databinding.FragmentMainBinding;
 import com.example.quickcode.main.adapters.RecyclerViewLanguagesAdapter;
@@ -96,6 +97,11 @@ public class MainFragment extends Fragment {
         tutorialsAdapter = new RecyclerViewTutorialsAdapter();
         tutorialsAdapter.setTutorialData(programmingTutorialDataList);
         binding.recyclerViewTutorials.setAdapter(tutorialsAdapter);
+
+        binding.bottomNavBar.add(new MeowBottomNavigation.Model(1, R.drawable.ic_quizzes));
+        binding.bottomNavBar.add(new MeowBottomNavigation.Model(2, R.drawable.ic_tutorials));
+        binding.bottomNavBar.add(new MeowBottomNavigation.Model(3, R.drawable.ic_exercises));
+        binding.bottomNavBar.show(2, true);
     }
 
 }
